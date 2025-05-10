3b0f1d1649fffc2499a58401aaef15668bf609f2
uknownanda <lakshmimounika.kakumuri@gmail.com>
2025-05-10 16:28:01 +0530

    Made game mobile responsive and added click-to-start support

--- app.js
+++ app.js
@@ -1,83 +1,88 @@
-let gameSeq=[];
-let userSeq=[];
+let gameSeq = [];
+let userSeq = [];
 
-let btns = ["red","yellow","green","purple"];
+let btns = ["red", "yellow", "green", "purple"];
 
 let started = false;
 let level = 0;
 
 let h2 = document.querySelector("h2");
 
-document.addEventListener("keypress",function(){ 
-    if(started == false){
-        started=true;
 
+function startGame() {
+    if (!started) {
+        started = true;
         levelUp();
-    }   
-})
+    }
+}
 
-function gameFlash(btn){
+document.addEventListener("keypress", startGame);
+document.addEventListener("click", startGame); 
+
+document.querySelectorAll(".btn").forEach(btn => {
+    btn.addEventListener("click", (e) => e.stopPropagation());
+});
+
+function gameFlash(btn) {
     btn.classList.add("flash");
-    setTimeout(function(){
+    setTimeout(() => {
         btn.classList.remove("flash");
     }, 250);
 }
 
-function userFlash(btn){
+function userFlash(btn) {
     btn.classList.add("userflash");
-    setTimeout(function(){
+    setTimeout(() => {
         btn.classList.remove("userflash");
     }, 250);
 }
 
-
-function levelUp(){
-    userSeq=[];
+function levelUp() {
+    userSeq = [];
     level++;
-    h2.innerText=`Level ${level}`;
+    h2.innerText = `Level ${level}`;
 
-    let randIdx = Math.floor(Math.random()*3);
+    let randIdx = Math.floor(Math.random() * 3);
     let randColor = btns[randIdx];
     let randbtn = document.querySelector(`.${randColor}`);
 
     gameSeq.push(randColor);
-    
     gameFlash(randbtn);
 }
 
-function checkAns(idx){
-    if(userSeq[idx]===gameSeq[idx]){
-        if(userSeq.length==gameSeq.length){
-            setTimeout(levelUp,1000);
+function checkAns(idx) {
+    if (userSeq[idx] === gameSeq[idx]) {
+        if (userSeq.length === gameSeq.length) {
+            setTimeout(levelUp, 1000);
         }
-    }else{
-        h2.innerHTML=`Game Over! Your score was <b>${level}</b><br>Press any key to start`;
-         document.querySelector("body").style.backgroundColor="red";
-        setTimeout(function(){
-            document.querySelector("body").style.backgroundColor="white";
-        },150)
+    } else {
+        h2.innerHTML = `Game Over! Your score was <b>${level}</b><br>Tap or press any key to restart`;
+        document.body.style.backgroundColor = "red";
+        setTimeout(() => {
+            document.body.style.backgroundColor = "white";
+        }, 150);
         reset();
     }
 }
 
-function btnPress(){
+function btnPress() {
     let btn = this;
     userFlash(btn);
 
-    userColor = btn.getAttribute("id");
+    let userColor = btn.getAttribute("id");
     userSeq.push(userColor);
 
-    checkAns(userSeq.length-1);
+    checkAns(userSeq.length - 1);
 }
 
 let allBtns = document.querySelectorAll(".btn");
-for(btn of allBtns){
-    btn.addEventListener("click",btnPress);
+for (let btn of allBtns) {
+    btn.addEventListener("click", btnPress);
 }
 
-function reset(){
+function reset() {
     started = false;
-    gameSeq=[];
-    userSeq-[];
-    level=0;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
 }
